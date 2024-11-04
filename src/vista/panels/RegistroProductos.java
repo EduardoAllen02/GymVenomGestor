aa8fac47e49d6c80fc7e381fcfe650b89dfecbe5
Lenovo <pa444257@uaeh.edu.mx>
2024-11-03 22:52:02 -0600

    registro de productos

---
+++ src/vista/panels/RegistroProductos.java
@@ -0,0 +1,10 @@
+package vista.panels;
+import modelo.Usuario;
+import modelo.ServicioUsuarios;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.time.LocalDate;
+public class RegistroProductos {
+    
+}
